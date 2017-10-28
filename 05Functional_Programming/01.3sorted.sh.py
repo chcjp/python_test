82f6577c02389836fcee92275ec3bfb71b4224d8
Jason Chen <chcjp@163.com>
2017-10-28 11:44:24 +0800

    modified
    sorted

--- 05Functional_Programming/01.3sorted.sh.py
+++ 05Functional_Programming/01.3sorted.sh.py
@@ -6,3 +6,25 @@ print('l:', l)
 
 l = sorted([36, 5, -12, 9, -21], key=abs)
 print('l by abs:', l)
+
+l = sorted(['bob', 'about', 'Zoo', 'Credit'], key=str.lower)
+print('str l:', l)
+
+# reverse sorted
+l = sorted(['bob', 'about', 'Zoo', 'Credit'], key=str.lower, reverse=True)
+print('str l2:', l)
+
+# practice
+L = [('Bob', 75), ('Adam', 92), ('Bart', 66), ('Lisa', 88)]
+def by_name(t):
+    return str.lower(t[0])
+    pass
+L2 = sorted(L, key=by_name)
+print('L2:', L2)
+
+def by_score(t):
+    return t[1]
+    pass
+
+L2 = sorted(L, key=by_score, reverse=True)
+print('L2 by score:', L2)
